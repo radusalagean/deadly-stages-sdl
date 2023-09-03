d9b2c377335590de694a77a3221528d7134072aa
Radu S <radusalagean@hotmail.com>
2023-09-03 13:54:06 +0300

    SDL Example added

--- main.cpp
+++ main.cpp
@@ -1,38 +1,51 @@
-#include <pspkernel.h>
-#include <pspdebug.h>
-#include <pspdisplay.h>
+#include <SDL.h>
 
-#define printf pspDebugScreenPrintf
-
-PSP_MODULE_INFO("DeadlyStagesDemo", 0, 1, 0);
-
-
-int exitCallback(int arg1, int arg2, void *common) 
+int main(int argc, char *argv[])
 {
-    sceKernelExitGame();
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
+
+    SDL_Window * window = SDL_CreateWindow(
+        "window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        480,
+        272,
+        0
+    );
+
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Rect square = {216, 96, 34, 64}; 
+
+    int running = 1;
+    SDL_Event event;
+    while (running) { 
+        if (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = 0;
+                    break;
+                case SDL_CONTROLLERDEVICEADDED:
+                    SDL_GameControllerOpen(event.cdevice.which);
+                    break;
+                case SDL_CONTROLLERBUTTONDOWN:
+                    if(event.cbutton.button == SDL_CONTROLLER_BUTTON_START)
+                        running = 0;
+                    break;
+            }
+        }
+
+        // Clear the screen
+        SDL_RenderClear(renderer);
+
+        // Draw a red square
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &square);
+
+        // Draw everything on a white background
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderPresent(renderer);
+    }
+
     return 0;
-}
-
-int callbackThread(SceSize args, void *argp) 
-{
-    int cbid = sceKernelCreateCallback("Exit Callback", exitCallback, NULL);
-    sceKernelRegisterExitCallback(cbid);
-    sceKernelSleepThreadCB();
-    return 0;
-}
-
-
-int setupCallbacks(void) 
-{
-    int thid = sceKernelCreateThread("update_thread", callbackThread, 0x11, 0xFA0, 0, 0);
-    sceKernelStartThread(thid, 0, 0);
-    return thid;
-}
-
-int main()
-{
-    setupCallbacks();
-    pspDebugScreenInit();
-    printf("DeadlyStagesDemo");
-    sceKernelSleepThread();
 }
