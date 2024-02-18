f65516a7fedca24c36c3ee30755100dc9f4c9b96
Radu Salagean <radusalagean@hotmail.com>
2024-02-18 14:51:30 +0200

    Copy assets task

--- src/Screen/MainMenu/MainMenuScreen.cpp
+++ src/Screen/MainMenu/MainMenuScreen.cpp
@@ -9,7 +9,7 @@ void MainMenuScreen::init()
 {
     IMG_Init(IMG_INIT_PNG);
     gScreenSurface = SDL_GetWindowSurface(Game::window);
-    SDL_Surface* imageSurface = SDL_LoadBMP("title_screen.bmp");
+    SDL_Surface* imageSurface = SDL_LoadBMP("res/image/title_screen.bmp");
     logd(SDL_GetError());
     texture = SDL_CreateTextureFromSurface(Game::renderer, imageSurface);
     SDL_FreeSurface(imageSurface);
