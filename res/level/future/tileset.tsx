63ff726cc9e4b0b113471ea098b662cbbd4c6581
Radu Salagean <radusalagean@outlook.com>
2024-10-27 09:57:19 +0200

    Future level

---
+++ res/level/future/tileset.tsx
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Tileset" tilewidth="32" tileheight="32" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="floor.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="tileroboti1b.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti2.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti3.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti3b.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti3c.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti3d.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti5.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti6.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti7.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti8.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti9.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti10.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti11.png" width="32" height="32"/>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti12.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="tileroboti12b.png" width="32" height="32"/>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+  <image source="wall.png" width="32" height="32"/>
+ </tile>
+</tileset>
