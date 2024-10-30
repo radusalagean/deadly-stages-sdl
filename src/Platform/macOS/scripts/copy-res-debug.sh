1cb23ad155ba22dd606ed87f7982e62ddda5f991
Radu Salagean <radusalagean@outlook.com>
2024-10-30 13:34:22 +0200

    mac compile script updated

--- src/Platform/macOS/scripts/copy-res-debug.sh
+++ src/Platform/macOS/scripts/copy-res-debug.sh
@@ -3,4 +3,16 @@
 source "$(dirname "${BASH_SOURCE[0]}")/pre-run.sh"
 
 mkdir -p build/macos-arm64-debug/res
-rsync -av --exclude '*.tiled-*' --exclude '.DS_Store' res/ build/macos-arm64-debug/res/
+rsync -av --exclude '*.tiled-*' \
+    --exclude '.DS_Store' \
+    --exclude 'level/*/preview_*.png' \
+    res/ build/macos-arm64-debug/res
+
+for dir in res/level/*; do
+    if [ -d "$dir" ]; then
+        level_name=$(basename "$dir")
+        if [ -f "$dir/preview_512.png" ]; then
+            cp "$dir/preview_512.png" "build/macos-arm64-debug/res/level/$level_name/preview.png"
+        fi
+    fi
+done
